8cfe55cd9b20e473d905476e4e2390f4f6281907
XenoProxy <xenoproxy@yandex.by>
2023-09-14 17:39:02 +0300

    welcome page

--- resources/views/device/show.blade.php
+++ resources/views/device/show.blade.php
@@ -6,7 +6,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">        
+    <div class="py-12"> 
+               
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">            
             <a class="btn btn-back" href="{{ route('dashboard') }}">Back</a>
 
@@ -20,9 +21,13 @@
                 </div>
     
                 <div class="device-indicators bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">                    
-                        <span>{{ $device_status }}</span>                              
-                    </div>              
+                    <div class="stat">
+                        <div class="status p-6 text-gray-900 dark:text-gray-100">{{ $device_status }}</div>
+                    </div>
+                    <div class="stat-bottom">
+                        <hr class="xunderline">                                             
+                        <div class="stat-name text-gray-900 dark:text-gray-100">Device status</div> 
+                    </div>                                 
                 </div>
 
                 <div class="device-indicators bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -30,8 +35,10 @@
                         <img class="temp-img" src="/images/free-icon-thermometer-696601.png">
                         <div class="p-6 text-gray-900 dark:text-gray-100">{{ $device->boiler_temp }}</div> 
                     </div>
-                                             
-                    <div class="stat-name text-gray-900 dark:text-gray-100">boiler temperature</div> 
+                    <div class="stat-bottom">
+                        <hr class="xunderline">                                             
+                        <div class="stat-name text-gray-900 dark:text-gray-100">Boiler temperature</div> 
+                    </div>                      
                 </div>
             </div>
         </div>        
